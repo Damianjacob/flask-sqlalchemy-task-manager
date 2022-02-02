4bae8f1210a8e29309cd82d8ae7b91b6cff19ebe
Damian Jacob <damianm.jacob@yahoo.com>
2022-02-02 12:36:12 +0000

    Extend basic template, add route for handling  form submission

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from taskmanager import app, db
 from taskmanager.models import Category, Task
 
@@ -8,3 +8,20 @@ from taskmanager.models import Category, Task
 def home():
     return render_template("tasks.html")
 
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+# We need POST for sending form data to the database and GET
+# to access data of the database and display it to the user
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method =="POST":
+        category = Category(category_name=request.form.get("category_name"))
+        db.session.add(category)
+        db.session.commit()
+        return redirect(url_for("categories"))
+    return render_template("add_category.html")
